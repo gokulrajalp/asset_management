56e860feacafa5f5659ac61f06f6a034a34920b3
gokulrajalp <gokulrajalp@gmail.com>
2024-02-16 11:35:31 +0530

    Node Js project created

---
+++ config.js
@@ -0,0 +1,12 @@
+const dbConfig = {
+    user: 'admin',
+    password: 'admin',
+    database: 'asset_management',
+    host: 'localhost',
+    port: 5432,
+    max: 10, // maximum number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+  
+  module.exports = dbConfig;
+  
