d955009fe8e09d9e8fb293279229f3e9db9e8c97
gokulrajalp <gokulrajalp@gmail.com>
2024-03-11 11:59:56 +0530

    Version 3 released

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
